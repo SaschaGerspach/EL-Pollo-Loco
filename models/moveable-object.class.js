0bfc853c1e6a4f8225520cefddb9713cb683067a
SaschaGerspach <sascha.gerspach@gmx.de>
2024-07-03 14:07:40 +0200

    animation

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,11 +4,22 @@ class MovableObject{
     img;
     height = 150;
     width = 100;
-
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.1;
+    otherDirection = false;
 
      loadImage(path){
         this.img = new Image();
         this.img.src = path;
+     }
+
+     loadImages(arr){
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
      }
 
     moveRight(){
@@ -16,6 +27,9 @@ class MovableObject{
     }
 
     moveLeft(){
-        
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
+        
 }
